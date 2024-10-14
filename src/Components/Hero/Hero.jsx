b47d865e65bb169899c5fa728f19fb9fffdfdb31
FAIZKHANMOHAMMED <nexusblade5195@gmail.com>
2024-10-14 21:04:43 +0530

    removed-khan

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import profile_img from '../../assets/profile_img.jpeg'
 export default function Hero () {
     return(<div className="hero">
        <div className="logo-box"><img src={profile_img} alt="" />
-       <h1><span>I'm Faiz</span> <br />Web developer </h1></div>
+       <h1><span>I'm Faiz K</span> <br />Web developer </h1></div>
        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos maxime excepturi expedita unde illum iure omnis eum! Corrupti hic, necessitatibus temporibus facere neque, delectus quas nobis incidunt repudiandae voluptas aperiam?</p>
         <div className="hero-action">
             <div className="hero-connect">connect with me</div>
